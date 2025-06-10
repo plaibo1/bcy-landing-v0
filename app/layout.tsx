70f2ef4a22c3413ed74f3f06d48ec290b6cb586b
plaibo1 <playboi.2000@mail.ru>
2025-06-10 20:54:26 +0300

    feat: update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "Банкротство физических лиц | Списание долгов законно | ЮрПомощь",
+  description:
+    "Поможем списать долги через банкротство физических лиц за 6-12 месяцев. Гарантия результата или возврат денег. Более 1056 успешных дел. Консультация бесплатно!",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
